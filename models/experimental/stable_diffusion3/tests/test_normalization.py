7edaa92e444c9d844efd5007e284091a1cb1267b
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2024-12-16 12:36:22 +0000

    add layer norm implementation

--- models/experimental/stable_diffusion3/tests/test_normalization.py
+++ models/experimental/stable_diffusion3/tests/test_normalization.py
@@ -6,36 +6,65 @@ import torch
 import ttnn
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
-from ..reference import SD3Transformer2DModel
 from ..reference.normalization import RmsNorm
-from ..tt.normalization import TtRmsNorm, TtRmsNormParameters
+from ..tt.normalization import TtLayerNorm, TtLayerNormParameters, TtRmsNorm, TtRmsNormParameters
 
 logger = logging.getLogger(__name__)
 
 
 @pytest.mark.parametrize(
-    "block_index, batch_size, input_dim",
+    "batch_size, input_dim",
     [
-        (0, 32, 128),
+        (32, 128),
+    ],
+)
+def test_layer_norm(
+    *,
+    device: ttnn.Device,
+    batch_size: int,
+    input_dim: int,
+):
+    torch_model = torch.nn.LayerNorm(input_dim, eps=1.0)
+
+    parameters = TtLayerNormParameters.from_torch(torch_model.state_dict(), device=device)
+    tt_model = TtLayerNorm(parameters, eps=torch_model.eps)
+
+    torch_input_tensor = torch.randn((batch_size, input_dim))
+
+    tt_input_tensor = ttnn.from_torch(
+        torch_input_tensor,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+    )
+
+    torch_output = torch_model(torch_input_tensor)
+
+    tt_output = tt_model(tt_input_tensor)
+    tt_output_torch = ttnn.to_torch(tt_output)
+
+    assert_with_pcc(torch_output, tt_output_torch, pcc=0.9999)
+
+
+@pytest.mark.parametrize(
+    "batch_size, input_dim",
+    [
+        (32, 128),
     ],
 )
 def test_rms_norm(
     *,
     device: ttnn.Device,
-    block_index: int,
     batch_size: int,
     input_dim: int,
 ):
-    parent_torch_model = SD3Transformer2DModel.from_pretrained(
-        "stabilityai/stable-diffusion-3.5-medium", subfolder="transformer"
-    )
-    torch_model: RmsNorm = parent_torch_model.transformer_blocks[block_index].attn.norm_q
-    torch_model.eval()
+    torch_model = RmsNorm(dim=input_dim, eps=1.0)
+    torch.nn.init.normal_(torch_model.weight)
 
     parameters = TtRmsNormParameters.from_torch(torch_model.state_dict(), device=device)
     tt_model = TtRmsNorm(parameters, eps=torch_model.eps)
 
-    torch_input_tensor = torch.randn((batch_size, input_dim, 64))
+    torch_input_tensor = torch.randn((batch_size, input_dim))
 
     tt_input_tensor = ttnn.from_torch(
         torch_input_tensor,
--- models/experimental/stable_diffusion3/tt/normalization.py
+++ models/experimental/stable_diffusion3/tt/normalization.py
@@ -33,8 +33,38 @@ class TtRmsNormParameters:
 
 @dataclass
 class TtLayerNormParameters:
-    weight: torch.Tensor | None
-    bias: torch.Tensor | None
+    weight: ttnn.Tensor | None
+    bias: ttnn.Tensor | None
+
+    @classmethod
+    def from_torch(
+        cls,
+        state: dict[str, torch.Tensor],
+        *,
+        dtype: ttnn.DataType | None = None,
+        device: ttnn.Device,
+    ) -> TtRmsNormParameters:
+        torch_weight = state["weight"]
+        torch_bias = state["bias"]
+
+        return cls(
+            weight=ttnn.from_torch(
+                torch_weight,
+                layout=ttnn.TILE_LAYOUT,
+                dtype=dtype,
+                device=device,
+            )
+            if torch_weight is not None
+            else None,
+            bias=ttnn.from_torch(
+                torch_bias,
+                layout=ttnn.TILE_LAYOUT,
+                dtype=dtype,
+                device=device,
+            )
+            if torch_bias is not None
+            else None,
+        )
 
 
 @dataclass
@@ -135,3 +165,15 @@ class TtRmsNorm:
         torch_x = torch_x * torch.rsqrt(variance + self._eps) * self._weight
 
         return ttnn.from_torch(torch_x, layout=x.layout, dtype=x.dtype, device=x.device())
+
+
+class TtLayerNorm:
+    def __init__(self, parameters: TtLayerNormParameters, *, eps: float) -> None:
+        super().__init__()
+
+        self._eps = eps
+        self._weight = parameters.weight
+        self._bias = parameters.bias
+
+    def __call__(self, x: ttnn.Tensor) -> ttnn.Tensor:
+        return ttnn.layer_norm(x, weight=self._weight, bias=self._bias, epsilon=self._eps)
