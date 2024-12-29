8bdd4d561160df6f3fcd6b2e486674bccdc599cc
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2024-12-29 19:09:13 +0000

    attention

--- models/experimental/stable_diffusion3/tt/timestep_embedding.py
+++ models/experimental/stable_diffusion3/tt/timestep_embedding.py
@@ -78,12 +78,7 @@ class _TimestepEmbedding:
 
     def __call__(self, x: ttnn.Tensor) -> ttnn.Tensor:
         x = self._linear_1(x)
-        # x = ttnn.silu(x) # imprecise
-        x = ttnn.from_torch(
-            torch.nn.functional.silu(ttnn.to_torch(x)),
-            device=x.device(),
-            layout=x.layout,
-        )
+        x = ttnn.silu(x)
         return self._linear_2(x)
 
 
