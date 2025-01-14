7f304d92cfe3b96380b90d0b5714bab543b10fc4
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-01-14 12:50:08 +0000

    improve normalization tests

--- models/experimental/stable_diffusion3/tests/test_normalization.py
+++ models/experimental/stable_diffusion3/tests/test_normalization.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import pytest
 import torch
 from loguru import logger
@@ -10,34 +12,26 @@ from ..tt.normalization import TtLayerNorm, TtLayerNormParameters, TtRmsNorm, Tt
 
 
 @pytest.mark.parametrize(
-    "batch_size, input_dim",
+    "input_shape",
     [
-        (32, 128),
+        [2, 24, 4096, 64],
     ],
 )
 def test_layer_norm(
     *,
     device: ttnn.Device,
-    batch_size: int,
-    input_dim: int,
+    input_shape: list[int],
 ):
     dtype = torch.bfloat16
 
-    torch_model = torch.nn.LayerNorm([input_dim], eps=1.0).to(dtype=dtype)
+    torch_model = torch.nn.LayerNorm(input_shape[-1:], eps=1.0).to(dtype=dtype)
 
     parameters = TtLayerNormParameters.from_torch(torch_model.state_dict(), device=device)
     tt_model = TtLayerNorm(parameters, eps=torch_model.eps)
 
-    torch_input_tensor = torch.randn(
-        (batch_size, input_dim),
-        dtype=dtype,
-    )
+    torch_input_tensor = torch.randn(input_shape, dtype=dtype)
 
-    tt_input_tensor = ttnn.from_torch(
-        torch_input_tensor,
-        device=device,
-        layout=ttnn.TILE_LAYOUT,
-    )
+    tt_input_tensor = ttnn.from_torch(torch_input_tensor, device=device, layout=ttnn.TILE_LAYOUT)
 
     with torch.no_grad():
         torch_output = torch_model(torch_input_tensor)
@@ -54,27 +48,25 @@ def test_layer_norm(
 
 
 @pytest.mark.parametrize(
-    "batch_size, input_dim",
+    "input_shape",
     [
-        (32, 128),
-        (10, 20),
+        [2, 24, 4096, 64],
     ],
 )
 def test_rms_norm(
     *,
     device: ttnn.Device,
-    batch_size: int,
-    input_dim: int,
+    input_shape: list[int],
 ):
     dtype = torch.bfloat16
 
-    torch_model = RmsNorm(dim=input_dim, eps=1.0).to(dtype=dtype)
+    torch_model = RmsNorm(dim=input_shape[-1], eps=1.0).to(dtype=dtype)
     torch.nn.init.normal_(torch_model.weight)
 
     parameters = TtRmsNormParameters.from_torch(torch_model.state_dict(), device=device)
     tt_model = TtRmsNorm(parameters, eps=torch_model.eps)
 
-    torch_input_tensor = torch.randn((batch_size, input_dim), dtype=dtype)
+    torch_input_tensor = torch.randn(input_shape, dtype=dtype)
 
     tt_input_tensor = ttnn.from_torch(torch_input_tensor, device=device, layout=ttnn.TILE_LAYOUT)
 
@@ -88,4 +80,4 @@ def test_rms_norm(
         tt_output_torch.to(dtype=torch.float32),
     ).item()
     logger.info(f"mse: {mse:.6f}")
-    assert_with_pcc(torch_output, tt_output_torch, pcc=0.999_95)
+    assert_with_pcc(torch_output, tt_output_torch, pcc=0.999_990)
