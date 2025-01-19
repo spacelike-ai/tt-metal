f39c31cdb72d4b3af75f33a698bf87a8bfdc3864
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-01-19 18:22:53 +0000

    enable tracing in transformer test

--- models/experimental/stable_diffusion3/tests/test_transformer.py
+++ models/experimental/stable_diffusion3/tests/test_transformer.py
@@ -7,6 +7,7 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
 
 from ..reference.transformer import SD3Transformer2DModel
 from ..tt.transformer import TtSD3Transformer2DModel, TtSD3Transformer2DModelParameters
+from ..tt.utils import allocate_tensor_on_device_like
 
 
 @pytest.mark.parametrize(
@@ -15,7 +16,7 @@ from ..tt.transformer import TtSD3Transformer2DModel, TtSD3Transformer2DModelPar
         (2, 333),
     ],
 )
-@pytest.mark.parametrize("device_params", [{"l1_small_size": 8192}], indirect=True)
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 8192, "trace_region_size": 15157248}], indirect=True)
 def test_transformer(
     *,
     device: ttnn.Device,
@@ -36,45 +37,52 @@ def test_transformer(
 
     torch.manual_seed(0)
     spatial = torch.randn((batch_size, 16, 64, 64), dtype=torch_dtype)
-    prompt_embed = torch.randn((batch_size, prompt_sequence_length, 4096), dtype=torch_dtype)
+    prompt = torch.randn((batch_size, prompt_sequence_length, 4096), dtype=torch_dtype)
     pooled_projection = torch.randn((batch_size, 2048), dtype=torch_dtype)
     timestep = torch.randint(1000, (batch_size,), dtype=torch.float32)
 
-    tt_spatial = ttnn.from_torch(
-        spatial.permute([0, 2, 3, 1]),  # BCYX -> BYXC
-        device=device,
-        layout=ttnn.TILE_LAYOUT,
-        dtype=ttnn_dtype,
-    )
-    tt_prompt_embed = ttnn.from_torch(
-        prompt_embed,
-        device=device,
-        layout=ttnn.TILE_LAYOUT,
-        dtype=ttnn_dtype,
-    )
-    tt_pooled_projection = ttnn.from_torch(
-        pooled_projection,
-        device=device,
-        layout=ttnn.TILE_LAYOUT,
-        dtype=ttnn_dtype,
-    )
-    tt_timestep = ttnn.from_torch(
-        timestep.unsqueeze(1),
-        device=device,
-        layout=ttnn.TILE_LAYOUT,
-    )
+    tt_spatial_host = ttnn.from_torch(
+        spatial.permute([0, 2, 3, 1]), layout=ttnn.TILE_LAYOUT, dtype=ttnn_dtype
+    )  # BCYX -> BYXC
+    tt_prompt_host = ttnn.from_torch(prompt, layout=ttnn.TILE_LAYOUT, dtype=ttnn_dtype)
+    tt_pooled_projection_host = ttnn.from_torch(pooled_projection, layout=ttnn.TILE_LAYOUT, dtype=ttnn_dtype)
+    tt_timestep_host = ttnn.from_torch(timestep.unsqueeze(1), layout=ttnn.TILE_LAYOUT)
 
     with torch.no_grad():
         torch_output = torch_model(
-            spatial=spatial, prompt_embed=prompt_embed, pooled_projections=pooled_projection, timestep=timestep
+            spatial=spatial, prompt_embed=prompt, pooled_projections=pooled_projection, timestep=timestep
         )
 
-    tt_output = tt_model(
+    tt_spatial = allocate_tensor_on_device_like(tt_spatial_host, device=device)
+    tt_prompt = allocate_tensor_on_device_like(tt_prompt_host, device=device)
+    tt_pooled_projection = allocate_tensor_on_device_like(tt_pooled_projection_host, device=device)
+    tt_timestep = allocate_tensor_on_device_like(tt_timestep_host, device=device)
+
+    # cache
+    tt_model(
         spatial=tt_spatial,
-        prompt_embed=tt_prompt_embed,
+        prompt_embed=tt_prompt,
         pooled_projection=tt_pooled_projection,
         timestep=tt_timestep,
     )
+
+    # trace
+    tid = ttnn.begin_trace_capture(device)
+    tt_output = tt_model(
+        spatial=tt_spatial,
+        prompt_embed=tt_prompt,
+        pooled_projection=tt_pooled_projection,
+        timestep=tt_timestep,
+    )
+    ttnn.end_trace_capture(device, tid)
+
+    # execute
+    ttnn.copy_host_to_device_tensor(tt_spatial_host, tt_spatial)
+    ttnn.copy_host_to_device_tensor(tt_prompt_host, tt_prompt)
+    ttnn.copy_host_to_device_tensor(tt_pooled_projection_host, tt_pooled_projection)
+    ttnn.copy_host_to_device_tensor(tt_timestep_host, tt_timestep)
+    ttnn.execute_trace(device, tid)
+
     tt_output_torch = ttnn.to_torch(tt_output)
 
     mse = torch.nn.functional.mse_loss(
