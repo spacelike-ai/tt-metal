8ed6f94cb4467d00d65aa8d4d63be7a977574a4d
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-01-31 21:11:50 +0000

    test non-joint attention as well

--- models/experimental/stable_diffusion3/tests/test_attention.py
+++ models/experimental/stable_diffusion3/tests/test_attention.py
@@ -26,6 +26,7 @@ if TYPE_CHECKING:
         (23, 2, 4096, 333),
     ],
 )
+@pytest.mark.parametrize("joint_attention", [False, True])
 @pytest.mark.parametrize("device_params", [{"trace_region_size": 517120}], indirect=True)
 @pytest.mark.usefixtures("use_program_cache")
 def test_attention(
@@ -35,6 +36,7 @@ def test_attention(
     batch_size: int,
     spatial_sequence_length: int,
     prompt_sequence_length: int,
+    joint_attention: bool,
 ) -> None:
     torch_dtype = torch.float32
     ttnn_dtype = ttnn.bfloat16
@@ -50,16 +52,16 @@ def test_attention(
 
     torch.manual_seed(0)
     spatial = torch.randn((batch_size, spatial_sequence_length, 1536), dtype=torch_dtype)
-    prompt = torch.randn((batch_size, prompt_sequence_length, 1536), dtype=torch_dtype)
+    prompt = torch.randn((batch_size, prompt_sequence_length, 1536), dtype=torch_dtype) if joint_attention else None
 
     tt_spatial_host = ttnn.from_torch(spatial, layout=ttnn.TILE_LAYOUT, dtype=ttnn_dtype)
-    tt_prompt_host = ttnn.from_torch(prompt, layout=ttnn.TILE_LAYOUT, dtype=ttnn_dtype)
+    tt_prompt_host = ttnn.from_torch(prompt, layout=ttnn.TILE_LAYOUT, dtype=ttnn_dtype) if joint_attention else None
 
     with torch.no_grad():
         spatial_output, prompt_output = torch_model(spatial=spatial, prompt=prompt)
 
     tt_spatial = allocate_tensor_on_device_like(tt_spatial_host, device=device)
-    tt_prompt = allocate_tensor_on_device_like(tt_prompt_host, device=device)
+    tt_prompt = allocate_tensor_on_device_like(tt_prompt_host, device=device) if joint_attention else None
 
     # cache
     tt_model(spatial=tt_spatial, prompt=tt_prompt)
@@ -71,11 +73,11 @@ def test_attention(
 
     # execute
     ttnn.copy_host_to_device_tensor(tt_spatial_host, tt_spatial)
-    ttnn.copy_host_to_device_tensor(tt_prompt_host, tt_prompt)
+    if joint_attention:
+        ttnn.copy_host_to_device_tensor(tt_prompt_host, tt_prompt)
     ttnn.execute_trace(device, tid)
 
     tt_spatial_output_torch = ttnn.to_torch(tt_spatial_output)
-    tt_prompt_output_torch = ttnn.to_torch(tt_prompt_output)
 
     mse = torch.nn.functional.mse_loss(
         spatial_output.to(dtype=torch.float32),
@@ -84,9 +86,12 @@ def test_attention(
     logger.info(f"spatial mse: {mse:.6f}")
     assert_with_pcc(spatial_output, tt_spatial_output_torch, pcc=0.990)
 
-    mse = torch.nn.functional.mse_loss(
-        prompt_output.to(dtype=torch.float32),
-        tt_prompt_output_torch.to(dtype=torch.float32),
-    ).item()
-    logger.info(f"prompt mse: {mse:.6f}")
-    assert_with_pcc(prompt_output, tt_prompt_output_torch, pcc=0.990)
+    if joint_attention:
+        tt_prompt_output_torch = ttnn.to_torch(tt_prompt_output)
+
+        mse = torch.nn.functional.mse_loss(
+            prompt_output.to(dtype=torch.float32),
+            tt_prompt_output_torch.to(dtype=torch.float32),
+        ).item()
+        logger.info(f"prompt mse: {mse:.6f}")
+        assert_with_pcc(prompt_output, tt_prompt_output_torch, pcc=0.990)
