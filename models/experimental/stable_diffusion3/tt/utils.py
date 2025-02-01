831b635c1223e431c067872c0608676913e7fe96
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-02-01 13:23:43 +0000

    remove unused function

--- models/experimental/stable_diffusion3/tt/utils.py
+++ models/experimental/stable_diffusion3/tt/utils.py
@@ -5,10 +5,5 @@
 import ttnn
 
 
-def increase_to_nearest_multiple(x: int, factor: int) -> int:
-    """Return smallest multiple of `factor` bigger or equal to `x`."""
-    return (x + factor - 1) // factor * factor
-
-
 def allocate_tensor_on_device_like(t: ttnn.Tensor, *, device: ttnn.Device) -> ttnn.Tensor:
     return ttnn.allocate_tensor_on_device(t.shape, t.dtype, t.layout, device)
