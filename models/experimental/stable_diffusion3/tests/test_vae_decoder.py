591e007ba83920019aadd4a92ef0c3561b24ba4e
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-02-01 13:15:41 +0000

    combine tests

--- models/experimental/stable_diffusion3/tests/test_vae_decoder.py
+++ models/experimental/stable_diffusion3/tests/test_vae_decoder.py
@@ -94,3 +94,91 @@ def test_vae_decoder(*, device: ttnn.Device, use_program_cache: bool, use_tracin
     ).item()
     logger.info(f"latent mse: {mse:.6f}")
     assert_with_pcc(image, tt_image_torch, pcc=0.990)
+
+
+@pytest.mark.parametrize("device_params", [{"trace_region_size": 40960}], indirect=True)
+@pytest.mark.parametrize(
+    ("use_program_cache", "use_tracing"),
+    [
+        (False, False),
+        (True, False),
+        (True, True),
+    ],
+)
+@pytest.mark.parametrize(
+    ("batch_size", "channels", "height", "width", "group_count"),
+    [
+        (1, 512, 16, 16, 32),
+        (1, 512, 32, 32, 32),
+        (1, 512, 64, 64, 32),
+        (1, 512, 128, 128, 32),
+        # (1, 512, 256, 256, 32),
+    ],
+)
+def test_vae_decoder_norm(
+    *,
+    device: ttnn.Device,
+    use_program_cache: bool,
+    use_tracing: bool,
+    batch_size: int,
+    channels: int,
+    height: int,
+    width: int,
+    group_count: int,
+) -> None:
+    if use_program_cache:
+        ttnn.enable_program_cache(device)
+
+    torch_dtype = torch.float32
+    ttnn_dtype = ttnn.bfloat16
+
+    torch_model = torch.nn.GroupNorm(num_groups=group_count, num_channels=channels)
+    torch_model.eval()
+
+    parameters = TtGroupNormParameters.from_torch(torch_model.state_dict(), device=device, dtype=ttnn_dtype)
+    tt_model = TtGroupNorm(parameters, num_groups=group_count, eps=torch_model.eps)
+
+    torch.manual_seed(0)
+
+    inp = torch.randn([batch_size, channels, height, width], dtype=torch_dtype)
+
+    tt_inp_host = ttnn.from_torch(inp.permute(0, 2, 3, 1), layout=ttnn.TILE_LAYOUT, dtype=ttnn_dtype)
+
+    with torch.no_grad():
+        out = torch_model(inp)
+
+    tt_inp = allocate_tensor_on_device_like(tt_inp_host, device=device)
+
+    if use_tracing:
+        # cache
+        logger.info("caching...")
+        tt_model(tt_inp)
+
+        # trace
+        logger.info("tracing...")
+        tid = ttnn.begin_trace_capture(device)
+        tt_out = tt_model(tt_inp)
+        ttnn.end_trace_capture(device, tid)
+
+        # execute
+        logger.info("executing...")
+        ttnn.copy_host_to_device_tensor(tt_inp_host, tt_inp)
+        ttnn.execute_trace(device, tid)
+        logger.info("done...")
+    else:
+        logger.info("compiling...")
+        tt_model(tt_inp)
+
+        logger.info("executing...")
+        ttnn.copy_host_to_device_tensor(tt_inp_host, tt_inp)
+        tt_out = tt_model(tt_inp)
+        logger.info("done...")
+
+    tt_out_torch = ttnn.to_torch(tt_out).permute(0, 3, 1, 2)
+
+    mse = torch.nn.functional.mse_loss(
+        out.to(dtype=torch.float32),
+        tt_out_torch.to(dtype=torch.float32),
+    ).item()
+    logger.info(f"mse: {mse:.6f}")
+    assert_with_pcc(out, tt_out_torch, pcc=0.999)
