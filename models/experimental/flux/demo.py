e538a81327a47b05712283649de78ec6fc28034d
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-04-13 18:31:24 +0200

    make demo not rely on pytest

--- models/experimental/flux/demo.py
+++ models/experimental/flux/demo.py
@@ -4,28 +4,28 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-import pytest
-
-from .tt import FluxPipeline
-
-if TYPE_CHECKING:
-    import ttnn
+import ttnn
+from models.experimental.flux.tt import FluxPipeline
 
 
-@pytest.mark.parametrize("device_params", [{"l1_small_size": 8192, "trace_region_size": 15210496}], indirect=True)
-@pytest.mark.usefixtures("use_program_cache")
-def test_flux(
-    *,
-    mesh_device: ttnn.MeshDevice,
-) -> None:
-    _, mesh_width = mesh_device.shape
+def main() -> None:
+    device_count = ttnn.get_num_devices()
+
+    mesh_shape = ttnn.MeshShape(1, min(2, device_count))
+    use_torch_encoder = device_count == 1
+
+    mesh_device = ttnn.open_mesh_device(
+        mesh_shape,
+        l1_small_size=8192,
+        trace_region_size=15210496,
+    )
+    for device in mesh_device.get_devices():
+        ttnn.enable_program_cache(device)
 
     pipeline = FluxPipeline(
         checkpoint="black-forest-labs/FLUX.1-schnell",
         device=mesh_device,
-        use_torch_encoder=mesh_width == 1,
+        use_torch_encoder=use_torch_encoder,
     )
 
     pipeline.prepare(width=1024, height=1024, prompt_count=1, num_images_per_prompt=1)
@@ -47,3 +47,7 @@ def test_flux(
         )
 
         images[0].save("flux_1024.png")
+
+
+if __name__ == "__main__":
+    main()
