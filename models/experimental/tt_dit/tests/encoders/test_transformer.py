bbd25dd79876111fb0c9dd5c1bfe114e3512ccbf
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-12-30 19:40:36 +0100

    relax accuracy requirement

--- models/experimental/tt_dit/tests/encoders/test_transformer.py
+++ models/experimental/tt_dit/tests/encoders/test_transformer.py
@@ -298,7 +298,7 @@ def test_guided_generation(*, mesh_device: ttnn.MeshDevice, skip_layers: int, ma
         # accuracy. It is not clear why.
         assert_quality(logits, tt_logits, ccc=0.9960, relative_rmse=0.09)
     else:
-        assert_quality(logits, tt_logits, ccc=0.9992, relative_rmse=0.05)
+        assert_quality(logits, tt_logits, ccc=0.9991, relative_rmse=0.05)
 
     assert tt_tokens_out.eq(tokens_out).all()
 
